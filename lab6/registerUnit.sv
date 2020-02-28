65ca4625ef7f2c7124d042ff06b44ab1d8defcb2
Kevin <kwhu5695@gmail.com>
2020-02-28 13:12:45 -0600

    new file, delete lab6_2.sv

---
+++ lab6/registerUnit.sv
@@ -0,0 +1,12 @@
+module registerUnit(
+                    input logic [2:0] DR_mux_in,
+                    input logic LD.REG,
+                    input logic [2:0] SR2,
+                    input logic [2:0] SR1_mux_in,
+                    input logic [15:0] bus_data,
+                    output logic [15:0] SR1_out, SR2_out
+);
+
+
+
+endmodule
