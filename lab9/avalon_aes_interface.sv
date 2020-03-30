9fb38fd1d1cd8b20f66d234d471cb360917e7b77
unknown <kwh2@illinois.edu>
2020-03-30 12:11:16 -0500

    lab9 week 1 almost done

---
+++ lab9/avalon_aes_interface.sv
@@ -0,0 +1,273 @@
+/************************************************************************
+Avalon-MM Interface for AES Decryption IP Core
+
+Dong Kai Wang, Fall 2017
+
+For use with ECE 385 Experiment 9
+University of Illinois ECE Department
+
+Register Map:
+
+ 0-3 : 4x 32bit AES Key
+ 4-7 : 4x 32bit AES Encrypted Message
+ 8-11: 4x 32bit AES Decrypted Message
+   12: Not Used
+	13: Not Used
+   14: 32bit Start Register
+   15: 32bit Done Register
+
+************************************************************************/
+
+module avalon_aes_interface (
+	// Avalon Clock Input
+	input logic CLK,
+	
+	// Avalon Reset Input
+	input logic RESET,
+	
+	// Avalon-MM Slave Signals
+	input  logic AVL_READ,					// Avalon-MM Read
+	input  logic AVL_WRITE,					// Avalon-MM Write
+	input  logic AVL_CS,						// Avalon-MM Chip Select
+	input  logic [3:0] AVL_BYTE_EN,		// Avalon-MM Byte Enable
+	input  logic [3:0] AVL_ADDR,			// Avalon-MM Address
+	input  logic [31:0] AVL_WRITEDATA,	// Avalon-MM Write Data
+	output logic [31:0] AVL_READDATA,	// Avalon-MM Read Data
+	
+	// Exported Conduit
+	output logic [31:0] EXPORT_DATA		// Exported Conduit Signal to LEDs
+);
+		
+		logic LE_KEY0, LE_KEY1, LE_KEY2, LE_KEY3,
+				LE_EN0, LE_EN1, LE_EN2, LE_EN3, 
+				LE_DE0, LE_DE1, LE_DE2, LE_DE3,
+				LE_START, LE_DONE;
+				
+		logic [127:0] AES_KEY, AES_MSG_EN, AES_MSG_DE;
+		logic [31:0] dumb_start, dumb_done;
+
+		
+		always_comb
+			begin
+				LE_KEY0 = 1'b0;
+				LE_KEY1 = 1'b0;
+				LE_KEY2 = 1'b0;
+				LE_KEY3 = 1'b0;
+				LE_EN0 = 1'b0;
+				LE_EN1 = 1'b0;
+				LE_EN2 = 1'b0;
+				LE_EN3 = 1'b0;
+				LE_DE0 = 1'b0;
+				LE_DE1 = 1'b0;
+				LE_DE2 = 1'b0;
+				LE_DE3 = 1'b0;
+				LE_START = 1'b0;
+				LE_DONE = 1'b0;
+				if (AVL_WRITE)
+					begin
+						case(AVL_ADDR)
+							4'b0000:
+								LE_KEY0 = 1'b1;
+							4'b0001:
+								LE_KEY1 = 1'b1;
+							4'b0010:
+								LE_KEY2 = 1'b1;
+							4'b0011:
+								LE_KEY3 = 1'b1;
+							4'b0100:
+								LE_EN0 = 1'b1;
+							4'b0101:
+								LE_EN1 = 1'b1;
+							4'b0110:
+								LE_EN2 = 1'b1;
+							4'b0111:
+								LE_EN3 = 1'b1;
+							4'b1000:
+								LE_DE0 = 1'b1;
+							4'b1001:
+								LE_DE1 = 1'b1;
+							4'b1010:
+								LE_DE2 = 1'b1;
+							4'b1011:
+								LE_DE3 = 1'b1;
+							//start and done
+							4'b1110:
+								LE_START = 1'b1;
+							4'b1111:
+								LE_DONE = 1'b1;
+								
+							default: ;
+								
+						endcase
+					end
+			end
+			
+			
+		always_comb
+			begin
+				EXPORT_DATA = 8'h00000000; 
+				if(AVL_READ)
+					begin
+						case(AVL_ADDR)
+							4'b0000:
+								EXPORT_DATA = AES_KEY[31:0];
+							4'b0001:
+								EXPORT_DATA = AES_KEY[63:32];
+							4'b0010:
+								EXPORT_DATA = AES_KEY[95:64];
+							4'b0011:
+								EXPORT_DATA = AES_KEY[127:0];
+							4'b0100:
+								EXPORT_DATA = AES_MSG_EN[31:0];
+							4'b0101:
+								EXPORT_DATA = AES_MSG_EN[63:32];
+							4'b0110:
+								EXPORT_DATA = AES_MSG_EN[95:64];
+							4'b0111:
+								EXPORT_DATA = AES_MSG_EN[127:0];
+							4'b1000:
+								EXPORT_DATA = AES_MSG_DE[31:0];
+							4'b1001:
+								EXPORT_DATA = AES_MSG_DE[63:32];
+							4'b1010:
+								EXPORT_DATA = AES_MSG_DE[95:64];
+							4'b1011:
+								EXPORT_DATA = AES_MSG_DE[127:0];
+							//start and done
+							4'b1110:
+								EXPORT_DATA = dumb_start;
+							4'b1111:
+								EXPORT_DATA = dumb_done;
+							default:
+								EXPORT_DATA = 8'h00000000; 
+						endcase
+					end
+			end
+		//AES_KEY
+ 		register32 AES_KEY0(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_KEY0), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_KEY[31:0])
+		);
+		
+		register32 AES_KEY1(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_KEY1), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_KEY[63:32])
+		);
+		
+		register32 AES_KEY2(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_KEY2), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_KEY[95:64])
+		);
+		
+		register32 AES_KEY3(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_KEY3), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_KEY[127:96])
+		);
+		
+		//AES_MEG_EN
+		register32 AES_MSG_EN0(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_EN0), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_EN[31:0])
+		);
+		
+		register32 AES_MSG_EN1(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_EN1), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_EN[63:32])
+		);
+		
+		register32 AES_MSG_EN2(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_EN2), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_EN[95:64])
+		);
+		
+		register32 AES_MSG_EN3(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_EN3), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_EN[127:96])
+		);
+		
+		//AES_MSG_DE
+		register32 AES_MSG_DE0(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE0), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_DE[31:0])
+		);
+		
+		register32 AES_MSG_DE1(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE1), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_DE[63:32])
+		);
+		
+		register32 AES_MSG_DE2(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE2), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_DE[95:64])
+		);
+		
+		register32 AES_MSG_DE3(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE3), .byte_enable(AVL_BYTE_EN), .data_in(AVL_WRITEDATA), 
+		.data_out(AES_MSG_DE[127:96])
+		);
+		
+		
+		//START & DONE
+		register32 START(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_START), .byte_enable(4'b0001), .data_in(AVL_WRITEDATA), 
+		.data_out(dumb_start)
+		);
+		
+		register32 DONE(
+		.Clk(CLK), .Reset(RESET), .load_enable(LE_DONE), .byte_enable(4'b0001), .data_in(AVL_WRITEDATA), 
+		.data_out(dumb_done)
+		);
+//*********************************************************************************************************//
+//AES_MSG_DE WEEK 2!!!!!!!!!!
+//		register32 AES_MSG_DE0(
+//		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE0), .byte_enable(AVL_BYTE_EN), .data_in(AES_MSG_DE[31:0]), 
+//		.data_out(AES_MSG_EN[31:0])
+//		);
+//		
+//		register32 AES_MSG_DE1(
+//		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE1), .byte_enable(AVL_BYTE_EN), .data_in(AES_MSG_DE[63:32]), 
+//		.data_out(AES_MSG_EN[63:32])
+//		);
+//		
+//		register32 AES_MSG_DE2(
+//		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE2), .byte_enable(AVL_BYTE_EN), .data_in(AES_MSG_DE[95:64]), 
+//		.data_out(AES_MSG_EN[95:64])
+//		);
+//		
+//		register32 AES_MSG_DE3(
+//		.Clk(CLK), .Reset(RESET), .load_enable(LE_DE3), .byte_enable(AVL_BYTE_EN), .data_in(AES_MSG_DE[127:96]), 
+//		.data_out(AES_MSG_EN[127:96])
+//		);
+//		
+//		AES aesbruh(
+//		.CLK(CLK), .RESET(RESET), .AES_START(), .AES_DONE(), .AES_KEY(), .AES_ENC(), .AES_DEC(AES_MSG_DE[127:0])
+//		);
+//*********************************************************************************************************//
+
+endmodule
+
+module register32 (
+		input logic Clk, Reset, load_enable, 
+		input logic [3:0] byte_enable,
+		input logic  [31:0] data_in,
+		output logic [31:0] data_out
+);
+			
+		always_ff @ (posedge Clk)
+		begin
+			if (Reset)
+				data_out <= 31'h0;
+			else if (load_enable)
+				begin
+					if(byte_enable[0])
+						data_out[7:0] <= data_in[7:0];
+					if(byte_enable[1])
+						data_out[15:8] <= data_in[15:8];
+					if(byte_enable[2])
+						data_out[23:16] <= data_in[23:16];
+					if(byte_enable[3])
+						data_out[31:24] <= data_in[31:24];
+				end
+		end
+		
+endmodule
---
+++ lab9/software/lab9software/main.c
@@ -0,0 +1,277 @@
+/************************************************************************
+Lab 9 Nios Software
+
+Dong Kai Wang, Fall 2017
+Christine Chen, Fall 2013
+
+For use with ECE 385 Experiment 9
+University of Illinois ECE Department
+************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "aes.h"
+
+// Pointer to base address of AES module, make sure it matches Qsys
+volatile unsigned int * AES_PTR = (unsigned int *) 0x00000080;
+
+// Execution mode: 0 for testing, 1 for benchmarking
+int run_mode = 0;
+
+
+void RotateWord(unsigned char * key, int i, unsigned char * result){
+	int index = 4*i;
+	result[0] = key[index + 3];
+	result[1] = key[index];
+	result[2] = key[index + 1];
+	result[3] = key[index + 2];
+}
+
+void KeyExpansion(unsigned char * key, unsigned char * keySchedule){
+	int i, j, k;
+	int rconCounter = 1;
+	for(i = 0; i < 11; i++){
+		for(j = 0; j < 4; j++){
+			unsigned char temp[4];
+			RotateWord(keySchedule, 4*i + 3+j, temp);
+			SubBytesButOnly4(temp);
+			for(k = 0; k < 4; k++){
+				if((j+1) % 4 == 0){
+					if(k == 0){
+						keySchedule[4*i + 3+j + k+4] = temp[k] ^ keySchedule[4*i + 3+j + k-12] ^ Rcon[rconCounter];
+						rconCounter++;
+					}
+					else{
+						keySchedule[4*i + 3+j + k+4] = temp[k] ^ keySchedule[4*i + 3+j + k-12];
+					}
+				}
+				else{
+					keySchedule[4*i + 3+j + k+4] = temp[k] ^ keySchedule[4*i + 3+j + k-12];
+				}
+			}
+		}
+	}
+
+}
+
+void SubBytesButOnly4(unsigned char * msg){
+	int i;
+	unsigned char upper, lower;
+	for(i = 0; i < 4; i++){
+		char upper = msg[i] >> 4;
+		char lower = msg[i] & 0x0F;
+		msg[i] = aes_sbox[upper * 0xF + lower];
+	}
+}
+
+void SubBytes(unsigned char * msg){
+	int i;
+	unsigned char upper, lower;
+	for(i = 0; i < 16; i++){
+		char upper = msg[i] >> 4;
+		char lower = msg[i] & 0x0F;
+		msg[i] = aes_sbox[upper * 0xF + lower];
+	}
+}
+
+void AddRoundKey(unsigned char * msg, unsigned char * curr_key){
+	int i;
+	for(i = 0; i < 16; i++){
+		msg[i] ^= curr_key[i];
+	} 
+}
+
+void ShiftRows(unsigned char * msg){
+	char temp0 = msg[1];
+	char temp1 = msg[5];
+	char temp2 = msg[9];
+	char temp3 = msg[13];
+	msg[1] = temp3;
+	msg[5] = temp0;
+	msg[9] = temp1;
+	msg[13] = temp3;
+
+	temp0 = msg[2];
+	temp1 = msg[6];
+	temp2 = msg[10];
+	temp3 = msg[14];
+	msg[2] = temp2;
+	msg[6] = temp3;
+	msg[10] = temp0;
+	msg[14] = temp1;
+
+	temp0 = msg[3];
+	temp1 = msg[7];
+	temp2 = msg[11];
+	temp3 = msg[15];
+	msg[3] = temp1;
+	msg[7] = temp2;
+	msg[11] = temp3;
+	msg[15] = temp0;
+}
+
+void MixColumns(unsigned char * msg){
+
+}
+
+
+
+/** charToHex
+ *  Convert a single character to the 4-bit value it represents.
+ *  
+ *  Input: a character c (e.g. 'A')
+ *  Output: converted 4-bit value (e.g. 0xA)
+ */
+char charToHex(char c)
+{
+	char hex = c;
+
+	if (hex >= '0' && hex <= '9')
+		hex -= '0';
+	else if (hex >= 'A' && hex <= 'F')
+	{
+		hex -= 'A';
+		hex += 10;
+	}
+	else if (hex >= 'a' && hex <= 'f')
+	{
+		hex -= 'a';
+		hex += 10;
+	}
+	return hex;
+}
+
+/** charsToHex
+ *  Convert two characters to byte value it represents.
+ *  Inputs must be 0-9, A-F, or a-f.
+ *  
+ *  Input: two characters c1 and c2 (e.g. 'A' and '7')
+ *  Output: converted byte value (e.g. 0xA7)
+ */
+char charsToHex(char c1, char c2)
+{
+	char hex1 = charToHex(c1);
+	char hex2 = charToHex(c2);
+	return (hex1 << 4) + hex2;
+}
+
+/** encrypt
+ *  Perform AES encryption in software.
+ *
+ *  Input: msg_ascii - Pointer to 32x 8-bit char array that contains the input message in ASCII format
+ *         key_ascii - Pointer to 32x 8-bit char array that contains the input key in ASCII format
+ *  Output:  msg_enc - Pointer to 4x 32-bit int array that contains the encrypted message
+ *               key - Pointer to 4x 32-bit int array that contains the input key
+ */
+void encrypt(unsigned char * msg_ascii, unsigned char * key_ascii, unsigned int * msg_enc, unsigned int * key)
+{
+	// Implement this function
+	int i;
+	unsigned char keySched[176];
+	unsigned char message_in[16];
+
+	// strncpy((char *)message_in, (char *)msg_ascii, 32);
+
+	for(i = 0; i < 16; i++){
+		message_in[i] = charsToHex(msg_ascii[2*i], msg_ascii[2*i+1]);
+		keySched[i] = charsToHex(key_ascii[2*i], key_ascii[2*i+1]);
+	}
+
+	KeyExpansion(key_ascii, keySched);
+
+	AddRoundKey(message_in, key_ascii);
+	// fix indexing!
+	for(i = 0; i < 9; i++){
+		SubBytes(message_in);
+		ShiftRows(message_in);
+		MixColumns(message_in);
+		AddRoundKey(message_in, keySched[i]);
+	}
+
+	SubBytes(message_in);
+	ShiftRows(message_in);
+	AddRoundKey(message_in, keySched[9]);
+}
+
+/** decrypt
+ *  Perform AES decryption in hardware.
+ *
+ *  Input:  msg_enc - Pointer to 4x 32-bit int array that contains the encrypted message
+ *              key - Pointer to 4x 32-bit int array that contains the input key
+ *  Output: msg_dec - Pointer to 4x 32-bit int array that contains the decrypted message
+ */
+void decrypt(unsigned int * msg_enc, unsigned int * msg_dec, unsigned int * key)
+{
+	// Implement this function
+}
+
+/** main
+ *  Allows the user to enter the message, key, and select execution mode
+ *
+ */
+int main()
+{
+	// Input Message and Key as 32x 8-bit ASCII Characters ([33] is for NULL terminator)
+	unsigned char msg_ascii[33];
+	unsigned char key_ascii[33];
+	// Key, Encrypted Message, and Decrypted Message in 4x 32-bit Format to facilitate Read/Write to Hardware
+	unsigned int key[4];
+	unsigned int msg_enc[4];
+	unsigned int msg_dec[4];
+
+	printf("Select execution mode: 0 for testing, 1 for benchmarking: ");
+	scanf("%d", &run_mode);
+
+	if (run_mode == 0) {
+		// Continuously Perform Encryption and Decryption
+		while (1) {
+			int i = 0;
+			printf("\nEnter Message:\n");
+			scanf("%s", msg_ascii);
+			printf("\n");
+			printf("\nEnter Key:\n");
+			scanf("%s", key_ascii);
+			printf("\n");
+			encrypt(msg_ascii, key_ascii, msg_enc, key);
+			printf("\nEncrpted message is: \n");
+			for(i = 0; i < 4; i++){
+				printf("%08x", msg_enc[i]);
+			}
+			printf("\n");
+			decrypt(msg_enc, msg_dec, key);
+			printf("\nDecrypted message is: \n");
+			for(i = 0; i < 4; i++){
+				printf("%08x", msg_dec[i]);
+			}
+			printf("\n");
+		}
+	}
+	else {
+		// Run the Benchmark
+		int i = 0;
+		int size_KB = 2;
+		// Choose a random Plaintext and Key
+		for (i = 0; i < 32; i++) {
+			msg_ascii[i] = 'a';
+			key_ascii[i] = 'b';
+		}
+		// Run Encryption
+		clock_t begin = clock();
+		for (i = 0; i < size_KB * 64; i++)
+			encrypt(msg_ascii, key_ascii, msg_enc, key);
+		clock_t end = clock();
+		double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		double speed = size_KB / time_spent;
+		printf("Software Encryption Speed: %f KB/s \n", speed);
+		// Run Decryption
+		begin = clock();
+		for (i = 0; i < size_KB * 64; i++)
+			decrypt(msg_enc, msg_dec, key);
+		end = clock();
+		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		speed = size_KB / time_spent;
+		printf("Hardware Encryption Speed: %f KB/s \n", speed);
+	}
+	return 0;
+}
